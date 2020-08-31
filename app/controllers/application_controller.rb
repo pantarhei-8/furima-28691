1137fc3c5b44a4599aa0365e798b76133518d6d1
pantarhei-8 <69030976+pantarhei-8@users.noreply.github.com>
2020-08-31 12:00:32 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # ログインしていないユーザーをログインページの画面に促す
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :first_name_reading, :name, :name_reading, :birth ])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :first_name, :name, :birth, presence: true
+  validates :first_name, presence: { message: 'Full-width characters'}
+  validates :name, presence: { message: 'Full-width characters'}
+  validates :password, presence: { message: 'Include both letters and numbers'}
+  
+  # with_optionsでdo~end内にpresence:trueを一括で設ける
+  with_options presence: true do
+    # withはカタカナの正規表現
+    validates :first_name_reading, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters'}
+    validates :name_reading, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters'}
+  end
+    
+
+
+
+
+
 end
