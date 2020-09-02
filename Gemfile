3e6ae0d8537b9c185eff20da7f01d8a93793e805
pantarhei-8 <69030976+pantarhei-8@users.noreply.github.com>
2020-09-02 15:05:54 +0900

    商品出品機能の実装
    ActiveStorage未実装

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 
+gem 'active_hash'
+
 group :production do
   gem 'rails_12factor'
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,24 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_image, :item_name, :item_text, :category_id, :status_id, :shipping_charger_id, :shipping_origin_id, :days_until_shipping_id, :price).merge(user_id: current_user.id)
+  end
+
+
 
 end
